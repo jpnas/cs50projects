6f77226014ae8b09bdf47f483a37e551a1a42edd
jpnas <jpnas@users.noreply.github.com>
2020-05-15 08:04:52 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -1,100 +1,57 @@
-//recovers lost JPEGs
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdint.h>
-#include<cs50.h>
+typedef uint8_t BYTE;
 
-
-
-//eliminate magic numbers
-#define JAR 512
-
-//making a struct
-typedef unsigned char BYTE;
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        fprintf(stderr, "Useage ./recover image name\n");
+        printf("Usage: ./recover image\n");
         return 1;
     }
+    
+    FILE *f = fopen(argv[1], "r");
 
-    //filename
-    char* card = argv[1];
-
-    // open input file
-    FILE* raw_data = fopen(card, "r");
-
-    if (raw_data == NULL)
+    int count = 0;
+    BYTE buffer[512];
+    char filename[8];
+    FILE *img;
+    
+    
+    while (fread(buffer, sizeof(buffer), 1, f) == 1)
     {
-        fprintf(stderr, "Could not open %s.\n", card);
-        return 2;
-    }
-
-    //buffer array
-    BYTE buffer[JAR];
-
-    //to count recovered img number
-    int counter;
-    counter = 0;
-
-    //file pointer i'll be writing to
-    FILE* img;
-
-    //name of jpeg is stored in this array 000.jpg\0
-    char picha[8];
-
-    while (fread(buffer, sizeof(buffer), 1, raw_data) == 1)
-    {
-
-        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        //Checking if the header is indicating a new JPEG
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            if (counter > 0)
+            //Check if it is the first JPEG and start writing
+            if (count == 0)
             {
-                //close the already open file
+                sprintf(filename, "%03i.jpg", count);
+                img = fopen(filename, "w");
+                fwrite(buffer, sizeof(buffer), 1, img);
+                count++;
+            }
+            //If it is not the first, close the current file and start the next
+            else if (count > 0)
+            {
                 fclose(img);
-
-                //name outfile using sprintf
-                sprintf(picha,"%03d.jpg", counter);
-                counter++;
-
-                //open newfile for writing
-                img = fopen(picha, "w");
-
-                // Write to outfile
+                sprintf(filename, "%03i.jpg", count);
+                img = fopen(filename, "w");
                 fwrite(buffer, sizeof(buffer), 1, img);
+                count++;
             }
-            if (counter == 0)
-            {
-                //name outfile using sprintf
-                sprintf(picha,"%03d.jpg", counter);
-                counter++;
-
-                //open newfile for writing
-                img = fopen(picha, "w");
-
-                // Write to outfile
-                fwrite(buffer, sizeof(buffer), 1, img);
-            }
-
-
-
         }
-        else if (counter > 0)
+        //If the header doesn't indicate a new JPEG, keep writing the current file
+        else if (count > 0)
         {
-
             fwrite(buffer, sizeof(buffer), 1, img);
         }
 
 
     }
-
-
     fclose(img);
-    fclose(raw_data);
-    //success
+    fclose(f);
     return 0;
-
 }
